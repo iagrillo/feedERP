ac1e9f5d82586d7b70440055d5bc575aee20c17c
Your Name <your.email@gmail.com>
2026-06-14 11:08:58 +0100

    Add Vercel build script with Flutter installation

---
+++ build.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1
+export PATH="./flutter/bin:"
+
+# Get Flutter version to verify installation
+flutter --version
+
+# Get dependencies
+flutter pub get
+
+# Build web
+flutter build web --release

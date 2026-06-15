9933e90091c417fae47b4edcbf4755add8fb25d6
Your Name <your.email@gmail.com>
2026-06-15 01:40:11 +0100

    Force rebuild: warehouse dropdown fix

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4191783503" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2544941875" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
